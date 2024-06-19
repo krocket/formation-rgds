60da7ed45faa0847d12d60ed7e4cb77ba2442387
edejean-rgds <edejeandelabatie@r-gds.fr>
2024-06-19 10:10:41 +0200

    init:ede

---
+++ library_app_ede/__manifest__.py
@@ -0,0 +1,9 @@
+{'name': 'Library EDE',
+'description': 'Bibliothèque de livres'
+'depends': ['base'],
+'author': 'edejean',
+'data': [],
+'demo': [],
+'application': 'True',
+'installable': True,
+}
