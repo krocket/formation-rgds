db305234964f05d8451b485c999b615ce563cca6
lkahl <lkahl@r-gds.fr>
2024-06-19 15:27:30 +0200

    librairie

--- library_app_lka/__manifest__.py
+++ library_app_lka/__manifest__.py
@@ -3,8 +3,8 @@
  'author': 'Laurent KAHL',
  'depends': ['base'],
  'data': [
-    'views/library_menu.xml',
     'views/book_view.xml',
+    'views/library_menu.xml',
  ],
  'demo': [],
  'application': True,
