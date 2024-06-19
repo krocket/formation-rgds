84ba48cc430dfdb0562e33013c459ad262e53bf5
aduringer <95362695+aduringer@users.noreply.github.com>
2024-06-19 15:28:38 +0200

    form book

--- library_app_adu/__manifest__.py
+++ library_app_adu/__manifest__.py
@@ -4,11 +4,12 @@
     "depends": ["base"],
     "author": "Alain DURINGER",
     "data": [
+        "view/library_books_view.xml",
         "view/library_menu.xml"
     ],
     "demo": [],
     "installable": True,
     "category": "Sales",
     "application": True,
-    "version": "15.0.1.0"
+    "version": "15.0.1.1"
 }
