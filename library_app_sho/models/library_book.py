3a121b9da4ed0861fe84fae319d35e738185ecb1
sho-rgds <shoehlinger@r-gds.fr>
2024-06-19 14:20:11 +0200

    Ajout modèle

---
+++ library_app_sho/models/library_book.py
@@ -0,0 +1,13 @@
+# function a+b
+from odoo import fields, models
+class Book(models.Model):
+    _name = 'library.book.sho'
+    _description = 'Book'
+
+    name = fields.Char(string='Title', required=True)
+    isbn = fields.Char(string='ISBN')
+    active = fields.Boolean(string='Active?', default=True)
+    date_published = fields.Date()
+    image = fields.Binary(string='Cover')
+    publisher_id = fields.Many2One('res.partner', string='Publisher')
+    author_ids = fields.Many2Many('res.partner', string='Authors')
