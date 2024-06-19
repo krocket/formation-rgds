93c142f6a5c5a1df7d90d663a564cb3fc75c472e
HOEHLINGER <SHOEHLINGER@r-gds.fr>
2024-06-19 10:14:14 +0200

    init_sho

---
+++ library_app_sho/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Mon appli Books SHO",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "SHO",
+    'description': """
+        Application Books SHO formation
+    """,
+    'data': [
+    ],
+    'demo': [
+    ],
+}
