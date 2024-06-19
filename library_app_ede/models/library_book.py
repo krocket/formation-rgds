47597f8f18128a6434536b48648bd3d36aa1bdc6
edejean-rgds <edejeandelabatie@r-gds.fr>
2024-06-19 16:15:59 +0200

    check_isbn

--- library_app_ede/models/library_book.py
+++ library_app_ede/models/library_book.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     image = fields.Binary(string='Cover')
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     authors_ids = fields.Many2many('res.partner', string='Authors')
-    def button_check_isbn(self):
+    def _check_isbn(self):
         self.ensure_one()
         digits = [int(x) for x in self.isbn if x.isdigit()]
         if len(digits) == 13:
@@ -19,3 +19,10 @@ class Book(models.Model):
             remain = sum(terms) % 10
             check = 10 - remain if remain != 0 else 0
             return digits[-1] == check
+    def button_check_isbn(self):
+            for book in self:
+                if not book.isbn:
+                    raise Warning('Please provide an ISBN for %s' % book.name)
+                if book.isbn and not book._check_isbn():
+                    raise Warning('%s is an invalid ISBN' % book.isbn)
+                return True
