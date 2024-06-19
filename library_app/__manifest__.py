1e7700fb19156ecdd62210a10b1b05a6d3f0d9e0
lkahl <lkahl@r-gds.fr>
2024-06-19 11:53:18 +0200

    erreur

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -5,7 +5,7 @@
  'data': [
     'security/library_security.xml',
     'security/ir.model.access.csv',
-
+    'views/library_menu.xml',
     'views/book_view.xml',
     'views/book_category_view.xml',
     'views/book_list_template.xml',
