62cbdd87f2c8c452780e97ed19f1e9bd5cb58641
aduringer <95362695+aduringer@users.noreply.github.com>
2024-06-19 14:19:55 +0200

    ajout book

---
+++ library_app_adu/models/library_book.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+class Book_ADU(models.model):
+    _name = 'library.book.ADU'
+    _description = 'Book'
+
+    name = fields.Char(string='Title', required=True)
+    isbn = fields.Char(string='ISBN')
+    active = fields.Boolean(string='Active?', default=True)
+    date_published = fields.Date()
+    image = fields.binary(string='Cover')
+    publisher_id = fields.Many2one('res.partner', string='Publisher')
+    author_ids = fields.Many2many('res.partner', string='Authors')
