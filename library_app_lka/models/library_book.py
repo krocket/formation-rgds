2a43fa60490b5e8adac2408d3d22145e7196ca5e
lkahl <lkahl@r-gds.fr>
2024-06-19 14:12:50 +0200

    model

---
+++ library_app_lka/models/library_book.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+class Book(models.Model):
+    _name = 'library.book.LKA'
+    _description = 'Book'
+
+
+name = fields.Char(string='Title',
+    help='Book cover title',
+    required=True)
+isbn = fields.Char(string='isbn')
+active = fields.boolean(string='active?', default=True)
+date_published = fields.date()
+image = fields.Binary(string='cover')
+publisher_id = fields.Many2one('res.partner', string='Publisher')
+author_ids = fields.Many2many('res.partner', string='Authors')
