30c46c58f18467abe2f2c3e80ea43fcb92bf60d5
edejean-rgds <edejeandelabatie@r-gds.fr>
2024-06-19 16:02:36 +0200

    add_button_isbn

--- library_app_ede/models/library_book.py
+++ library_app_ede/models/library_book.py
@@ -10,3 +10,12 @@ class Book(models.Model):
     image = fields.Binary(string='Cover')
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     authors_ids = fields.Many2many('res.partner', string='Authors')
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderations = [1, 3] * 6
+            terms = [a * b for a, b in zip(digits[:12], ponderations)]
+            remain = sum(terms) % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
