e0c70dde675585671f5c5143c26f1f9ffca3c277
KAHL <LKAHL@r-gds.fr>
2024-06-19 10:11:24 +0200

    init : LKA

---
+++ library_app_lka/__manifest__.py
@@ -0,0 +1,11 @@
+{'name': 'Librairie LKA',
+ 'description': 'Librairie',
+ 'author': 'Laurent KAHL',
+ 'depends': ['base'],
+ 'data': [
+  ],
+ 'demo': [
+  ],
+ 'application': True,
+ 'installable': True,
+}
