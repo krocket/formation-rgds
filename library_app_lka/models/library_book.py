98294a876ec3b6c21f5355dd433f86dca74ac601
lkahl <lkahl@r-gds.fr>
2024-06-19 14:30:07 +0200

    librairie

--- library_app_lka/models/library_book.py
+++ library_app_lka/models/library_book.py
@@ -3,12 +3,10 @@ class Book(models.Model):
     _name = 'library.book.lka'
     _description = 'Book'
 
-name = fields.Char(string='Title',
-    help='Book cover title',
-    required=True)
-isbn = fields.Char(string='isbn')
-active = fields.Boolean(string='active?', default=True)
-date_published = fields.Date()
-image = fields.Binary(string='cover')
-publisher_id = fields.Many2one('res.partner', string='Publisher')
-author_ids = fields.Many2many('res.partner', string='Authors')
+    name = fields.Char(string='Title', help='Book cover title', required=True)
+    isbn = fields.Char('ISBN')
+    active = fields.Boolean(string='active?', default=True)
+    date_published = fields.Date()
+    image = fields.Binary(string='cover')
+    publisher_id = fields.Many2one('res.partner', string='Publisher')
+    author_ids = fields.Many2many('res.partner', string='Authors')
