2c283f880e56f43cd3597b2b54a32ca3437f6064
lkahl <lkahl@r-gds.fr>
2024-06-19 15:57:56 +0200

    librairie

--- library_app_lka/models/library_book.py
+++ library_app_lka/models/library_book.py
@@ -10,3 +10,13 @@ class Book(models.Model):
     image = fields.Binary(string='cover')
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
+
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderators = [1, 3] * 6
+            total = sum(a * b for a, b in zip(digits[:12], ponderators))
+            remain = total % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
