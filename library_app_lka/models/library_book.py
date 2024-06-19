30120273f7b5f07a54847c019c7321b0932b5f94
lkahl <lkahl@r-gds.fr>
2024-06-19 16:24:45 +0200

    librairie

--- library_app_lka/models/library_book.py
+++ library_app_lka/models/library_book.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-from odoo.exceptions import Warning
+from odoo.exceptions import UserError
 class Book(models.Model):
     _name = 'library.book.lka'
     _description = 'Book'
@@ -25,7 +25,7 @@ class Book(models.Model):
     def button_check_isbn(self):
       for book in self:
         if not book.isbn:
-            raise Warning('Please provide an ISBN13 for %s' % book.name)
+            raise UserError('Please provide an ISBN13 for %s' % book.name)
         if book.isbn and not book._check_isbn():
-            raise Warning('%s is an invalid ISBN' % book.isbn)
+            raise UserError('%s is an invalid ISBN' % book.isbn)
       return True
