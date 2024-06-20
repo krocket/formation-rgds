9397f34fe1b7f9b0a4ef776ecf4ab94d30eaba5a
edejean-rgds <edejeandelabatie@r-gds.fr>
2024-06-20 10:36:16 +0200

    security2

--- library_app_ede/__manifest__.py
+++ library_app_ede/__manifest__.py
@@ -3,11 +3,12 @@
  'depends': ['base', 'sales_team'],
  'author': 'edejean',
  'data': [
+    'security/library_security.xml',
     'views/library_books_view.xml',
     'views/library_menu.xml',
 ],
  'demo': [],
  'application': True,
  'installable': True,
- 'version': '15.0.1.15',
+ 'version': '15.0.1.16',
 }
