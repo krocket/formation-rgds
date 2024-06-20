6261a36dd801aa79d86442153d9a8c2cb5ec5bd7
lkahl <lkahl@r-gds.fr>
2024-06-20 14:00:25 +0200

    librairie member

---
+++ library_member_lka/__manifest__.py
@@ -0,0 +1,12 @@
+{
+'name': 'Library Members LKA',
+'description': 'librairie members LKA.',
+'author': 'Laurent KAHL',
+'data': [],
+'depends': [
+    'library_app_lka',
+    'mail',
+],
+'application': False,
+}
+
