0f0357a3c6518841e59c9eb8416728d8557512be
lkahl <lkahl@r-gds.fr>
2024-06-20 14:33:41 +0200

    library

--- library_member_lka/__manifest__.py
+++ library_member_lka/__manifest__.py
@@ -2,7 +2,9 @@
 'name': 'Library Members LKA',
 'description': 'librairie members LKA.',
 'author': 'Laurent KAHL',
-'data': [],
+'data': [
+    'views/book_view.xml',
+],
 'depends': [
     'library_app_lka',
     'mail',
