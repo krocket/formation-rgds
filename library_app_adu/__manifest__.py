3738214fa4fe8142e5584247b7768d1e9fd2a745
aduringer <95362695+aduringer@users.noreply.github.com>
2024-06-20 11:15:01 +0200

    securité 3

--- library_app_adu/__manifest__.py
+++ library_app_adu/__manifest__.py
@@ -6,11 +6,11 @@
     "data": [
         "security/library_security.xml",
         "view/library_books_view.xml",
-        "view/library_menu.xml",
+        "view/library_menu.xml"
     ],
     "demo": [],
     "installable": True,
     "category": "Sales",
     "application": True,
-    "version": "15.0.1.13"
+    "version": "15.0.1.14"
 }
